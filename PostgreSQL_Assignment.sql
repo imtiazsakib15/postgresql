1144eec163ca69e666f53b792e96a98da5c4a595
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-05-27 16:39:32 +0600

    insertion data command done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -28,3 +28,29 @@ CREATE TABLE enrollment (
     course_id INT NOT NULL REFERENCES courses(course_id)
 );
 
+-- Insert sample data into students table
+INSERT into students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES ('Sameer',   21,	'sameer@example.com',	48,	60,	NULL),
+       ('Zoya',	    23,	'zoya@example.com',    	52,	58,	NULL),
+       ('Nabil',	22,	'nabil@example.com',	37,	46,	NULL),
+	   ('Rafi',	    24,	'rafi@example.com',	    41,	40,	NULL),
+	   ('Sophia',	22,	'sophia@example.com',	50,	52,	NULL),
+       ('Hasan',	23,	'hasan@gmail.com',      43,	39,	NULL);
+
+-- Insert sample data into courses table
+INSERT into courses (course_name, credits)
+VALUES ('Next.js', 3),
+       ('React.js', 4),
+       ('Databases', 3),
+       ('Prisma', 3);
+
+-- Insert sample data into enrollment table
+INSERT into enrollment (student_id, course_id)
+VALUES (1, 1),
+       (1, 2),
+       (2, 1),
+       (3, 2);
+
+SELECT * FROM students;
+SELECT * FROM courses;
+SELECT * FROM enrollment;
